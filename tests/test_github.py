1d2901f1d05f14a6bd01d227fc35f438d54a9326
Marco <konkerpl>
2022-01-17 22:41:14 -0600

    Configuration of Selenium WD

--- tests/test_github.py
+++ tests/test_github.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome(executable_path='../pythonProject/drivers/chromedriver')
-
-driver.get('http://www.google.com')
 
 
-driver.close()
+def test_pytest_guthub():
+    chrome_driver = webdriver.Chrome(executable_path='../pythonProject/drivers/chromedriver')
+
+    chrome_driver.get('http://github.com')
+    chrome_driver.maximize_window()
+
+    chrome_driver.close()
