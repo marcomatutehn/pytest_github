4ffa2911eabc6683209350d22da25f95cbe0e9ef
Marco <konkerpl>
2022-01-18 22:00:21 -0600

    1. Open [https://github.com/](https://github.com/). 2. Focus the `Search GitHub` search bar. 3. Input `react` and submit. 4. In the search results, find `Advanced Search` and click it.

--- tests/test_github.py
+++ tests/test_github.py
@@ -1,10 +1,27 @@
+import time
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+SCROLL_PAUSE_TIME = 0.5
 
 
 def test_pytest_guthub():
-    chrome_driver = webdriver.Chrome(executable_path='../pythonProject/drivers/chromedriver')
+    driver = webdriver.Chrome(executable_path='../pythonProject/drivers/chromedriver')
 
-    chrome_driver.get('http://github.com')
-    chrome_driver.maximize_window()
+    driver.get('http://github.com')
+    driver.maximize_window()
 
-    chrome_driver.close()
+    search_box = driver.find_element(By.NAME, 'q')
+    search_box.send_keys("react")
+    search_box.send_keys(Keys.ENTER)
+
+    driver.execute_script("window.scrollTo(0, 500)")
+
+    btn_advance_search = driver.find_element(By.CSS_SELECTOR, 'a.f6')
+    btn_advance_search.click()
+
+    time.sleep(SCROLL_PAUSE_TIME)
+
+    driver.close()
